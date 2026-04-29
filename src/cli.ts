7565137957d76407ebff3009a98ee749791caebf
이동규 (Justin) <nninnnin7@gmail.com>
2026-04-29 19:25:36 +0900

    Implement file watcher in CLI and add TypeScript configuration

--- src/cli.ts
+++ src/cli.ts
@@ -1,1 +1,18 @@
+import chokidar from "chokidar";
+
 console.log("Hummer cli: Hi");
+
+const projectPath =
+  "/Users/donggyulee/.claude/projects/-Users-donggyulee-Projects-hummer-cli";
+
+const watcher = chokidar.watch(projectPath, {
+  persistent: true,
+});
+
+watcher.on("add", (path) => {
+  console.log(`File ${path} has been added`);
+});
+
+watcher.on("change", (path) => {
+  console.log(`File ${path} has been changed`);
+});
